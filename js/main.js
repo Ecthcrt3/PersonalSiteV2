a4b733db167d8aaedde9b397c625250102daf3b8
Gene Cathcart <ecthcrt3@gmail.com>
2023-07-31 18:11:38 -0500

    fixed api display updated zoeys card

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ var country = 'us'
 var year = `${today.getFullYear()}`
 let displayHoliday = document.getElementById('holidayAPI');
 
-console.log(year)
+
 $.ajax({
     method: 'GET',
     url: 'https://api.api-ninjas.com/v1/holidays?country=' + country + '&year=' + year,
@@ -19,13 +19,13 @@ $.ajax({
     success: function(result) {
         let sortedResult = result.sort((d1, d2) => (new Date(d1.date) < new Date(d2.date) ? 1 : (new Date(d1.date) > new Date(d2.date)) ? -1 : 0)).reverse();
         sortedResult = sortedResult.filter(holiday => new Date(holiday.date) >= today)[0];
-        let nextHoliday = new Date(sortedResult.date)
-        nextHoliday.setDate(nextHoliday.getDate())
         displayHoliday.innerHTML = `<h2>The next holiday is ${sortedResult.name}!</h2>
         <br>
-        <h3>${sortedResult.day} ${nextHoliday.toLocaleString('default', { month: 'long' })} ${nextHoliday.getDay()}</h3>`
+        <h3>${sortedResult.day} ${sortedResult.date}</h3>`
     },
     error: function ajaxError(jqXHR) {
         console.error('Error: ', jqXHR.responseText);
     }
 });
+
+
